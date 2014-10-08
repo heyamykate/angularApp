925d2ab60a92280a468633a997226dd02a4520d9
Amy Andrews <amy.kate.andrews@gmail.com>
2014-10-07 19:11:38 -0600

    added ng repeat stuff

--- app.js
+++ app.js
@@ -4,4 +4,30 @@ myApp.controller('MainCtrl', ['$scope', function($scope) {
     
     $scope.test = "Hello AK!";
     
+    $scope.friends = [
+        {
+            name: 'Jake',
+            occupation: 'Wizard',
+            location: 'Idaho'
+        },
+        
+        {
+            name: 'Dan',
+            occupation: 'Dragon Slayer',
+            location: 'Narnia'
+        },
+        
+        {
+            name: 'Kelly',
+            occupation: "Knight's Watch",
+            location: 'The Wall'
+        },
+        
+        {
+            name: 'James',
+            occupation: 'Wizard Apprentice',
+            location: 'Dreamland'
+        }
+    ];
+    
 }]);
